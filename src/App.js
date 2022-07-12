d258dd87db5b77c03e7984b45dc6b961adeed1c8
Chisom Ogbonna <mavreon@gmail.com>
2022-07-12 23:24:25 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React,{useState} from "react";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
 function App() {
+  const [users, setUsers] = useState([]);
+  
+  const onAddUserHandler = (object)=>{
+    //console.log(object);
+    //users.push(object);
+    setUsers((prevState)=>{
+      return [object,...prevState];
+    });
+    //console.log(users);
+  }
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <AddUser onAddUser = {onAddUserHandler}></AddUser>
+      <UsersList users ={users}></UsersList>
     </div>
   );
 }
---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,59 @@
+import React,{useState} from "react";
+import styles from './AddUser.module.css';
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
+
+const AddUser = (props)=>{
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
+    const addUserHandler=(event)=>{
+        event.preventDefault();
+        if(enteredUsername.trim().length ===0 || +enteredAge === 0){
+            return;
+        };
+        if(+enteredAge < 1){
+            return;
+        }
+        //console.log(enteredUsername, enteredAge);
+        const object = {
+            id: Math.random(),
+            name: enteredUsername, 
+            age: enteredAge
+        };
+
+        props.onAddUser(object);
+        setEnteredUsername('');
+        setEnteredAge('');
+    };
+
+    const usernameChangeHandler =(event)=>{
+        setEnteredUsername(event.target.value);
+    };
+
+    const ageChangeHandler =(event)=>{
+        setEnteredAge(event.target.value);
+    };
+
+    const clickHandler=(event)=>{
+        console.log('Clicked!');
+    };
+
+    return(
+        <div>
+            <ErrorModal title="An error occured" message="Something is wrong!"></ErrorModal>
+            <Card className={styles.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input id="username" type="text" onChange={usernameChangeHandler} value = {enteredUsername}/>
+                    <label htmlFor="age">Age (Years)</label>
+                    <input id="age" type="number" onChange={ageChangeHandler} value = {enteredAge}/>
+                    <Button type="submit" onClick={clickHandler}>Add User</Button>
+                </form>
+            </Card>
+        </div>
+    );
+};
+
+export default AddUser;
